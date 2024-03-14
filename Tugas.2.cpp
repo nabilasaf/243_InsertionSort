08f77ce847f332a00c0f0066b9f508443aca3db2
nabilasaf <nabilasafitri861@gmail.com>
2024-03-14 15:12:23 +0700

    memasukkan display

--- Tugas.2.cpp
+++ Tugas.2.cpp
@@ -38,13 +38,28 @@ void insertionSort() {
         temp = arr[i]; //step 2
 
         j = i - 1; // step 3
-        
+
         while (j >= 0 && arr[j] > temp) // step 4
         {
             arr[j + 1] = arr[j]; //step 4a
             j = j - 1; //step 4b
         }
         arr[j + 1] = temp; //step 5
+    }
+}
+
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+    }
+    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
+}
 
 int main()
 {
