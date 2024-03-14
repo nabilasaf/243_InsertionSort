70eeebcd55c66258588582e4adfaa086b1418f71
nabilasaf <nabilasafitri861@gmail.com>
2024-03-14 15:09:38 +0700

    memasukkan step 4

--- Tugas.2.cpp
+++ Tugas.2.cpp
@@ -38,6 +38,12 @@ void insertionSort() {
         temp = arr[i]; //step 2
 
         j = i - 1; // step 3
+        
+        while (j >= 0 && arr[j] > temp) // step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j = j - 1; //step 4b
+        }
 
 int main()
 {
